61e63ac9edb0b904d65dfc70ebe01089b16fc373
vindhyesh pandey <pandey.vindhyesh@gmail.com>
2026-08-26 00:40:20 +0530

    secret issue resolved

---
+++ environments/dev/output.tf
@@ -0,0 +1,7 @@
+# output "terraform_identity_object_id" {
+#   value = data.azurerm_client_config.current.object_id
+# }
+
+# output "terraform_identity_tenant_id" {
+#   value = data.azurerm_client_config.current.tenant_id
+# }
